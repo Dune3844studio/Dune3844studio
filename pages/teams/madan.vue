2a57e18b5ba18c9c249a9e5ffe141c4079d25292
kantanishio <kantanishio@mg-oxide.com>
2023-01-10 03:07:14 +0900

    madan

--- pages/teams/madan.vue
+++ pages/teams/madan.vue
@@ -10,6 +10,7 @@
        <div class="mb-10 md:mb-16">
          <h2 class="text-blue-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Madan</h2>
       
+         <h2 class="text-blue-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">King of MANGO</h2>
        </div>
      </div>
    </div>
