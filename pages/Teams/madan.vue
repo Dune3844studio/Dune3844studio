4aa9c39d43ddf83a5c862a70e6b16c679027761e
Dune3844studio <dunestudio3844@gmail.com>
2023-01-10 02:24:45 +0900

    sudo

---
+++ pages/Teams/madan.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+
+
+
+
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+       <!-- text - start -->
+       <div class="mb-10 md:mb-16">
+         <h2 class="text-blue-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Madan</h2>
+      
+       </div>
+     </div>
+   </div>
+   </template>
+     
