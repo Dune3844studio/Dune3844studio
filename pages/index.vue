02620040b10208a1b4801cc9d8419b1521438238
Dune3844studio <dunestudio3844@gmail.com>
2023-01-10 20:51:39 +0900

    いんでx

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,87 @@
 <template>
  
 
+ 
 
 
-
- <div class="bg-white py-6 sm:py-8 lg:py-12">
-  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-    <!-- text - start -->
-    <div class="mb-10 md:mb-16">
-      <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">ギじいっじじjらりー</h2>
-
-      <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
-    </div>
-    <!-- text - end -->
-
-    <div class="grid grid-cols-2 sm:grid-cols-3 gap-4 md:gap-6 xl:gap-8">
-      <!-- image - start -->
-      <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
-        <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-
-        <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
-
-        <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">VR</span>
-      </a>
-      <!-- image - end -->
-
-      <!-- image - start -->
-      <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
-        <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-
-        <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
-
-        <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">Retro</span>
-      </a>
-      <!-- image - end -->
-
-      <!-- image - start -->
-      <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
-        <img src="@/static/collection/20200816-OHT1I50196-L.jpg" loading="lazy" alt="Photo by Magicle" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
-
-        <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
-
-        <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">Pregnant</span>
-      </a>
-      <!-- image - end -->
-    </div>
-  </div>
-</div>
-</template>
-  
+  <div class="bg-white py-6 sm:py-8 lg:py-12">
+   <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+     <!-- text - start -->
+    
+     <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+ 
+   <video autoplay loop controls class="absolute z-10 w-full min-w-full min-h-full max-w-none">
+     <source src="@/static/movie/政策決定PR0109_2.mp4" type="video/mp4" />Your browser does not support the video tag.
+   </video>
+ </header>
+ <div class="max-w-lg m-auto">
+   <p class="mb-4">
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
+     magna non varius. Proin leo felis, euismod non porta eget, varius sit amet
+     sapien. Maecenas in nulla at leo convallis consectetur id a sapien. Nulla
+     nec pulvinar nisi. Vivamus non facilisis lacus, et volutpat libero. Nulla ac
+     odio aliquam, accumsan arcu ut, lacinia est. Nulla eu sem elit. Fusce nec
+     laoreet sem, semper molestie libero.
+   </p>
+   <p class="mb-4">
+     Ut sagittis lacus consequat accumsan venenatis. Sed sollicitudin, lectus et
+     fringilla ultrices, dolor nisi scelerisque tortor, vel finibus magna massa
+     non nunc. Phasellus massa quam, egestas a nisl sed, porta volutpat metus.
+     Nunc sed elit ac tellus tempor cursus. Suspendisse potenti. Vestibulum
+     varius rutrum nisl nec consequat. Suspendisse semper dignissim sem viverra
+     semper. Nulla porttitor, purus nec accumsan pharetra, nisi dolor condimentum
+     ipsum, id consequat nulla nunc in ligula.
+   </p>
+   <p class="mb-12">
+     Nulla pharetra lacinia nisi, vitae mollis tellus euismod id. Mauris porta
+     dignissim hendrerit. Cras id velit varius, fermentum lectus vitae, ultricies
+     dolor. In bibendum rhoncus purus vel rutrum. Nam vulputate imperdiet
+     fringilla. Donec blandit libero massa. Suspendisse dictum diam mauris, vitae
+     fermentum dolor tincidunt in. Pellentesque sollicitudin venenatis dolor,
+     vitae scelerisque elit ultrices eu. Donec eget sodales risus, quis dignissim
+     neque.
+   </p>
+ </div>
+    
+     <div class="mb-10 md:mb-16">
+       <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Gallery</h2>
+ 
+       <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+     </div>
+     <!-- text - end -->
+ 
+     <div class="grid grid-cols-2 sm:grid-cols-3 gap-4 md:gap-6 xl:gap-8">
+       <!-- image - start -->
+       <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
+         <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+ 
+         <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
+ 
+         <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">VR</span>
+       </a>
+       <!-- image - end -->
+ 
+       <!-- image - start -->
+       <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
+         <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+ 
+         <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
+ 
+         <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">Retro</span>
+       </a>
+       <!-- image - end -->
+ 
+       <!-- image - start -->
+       <a href="#" class="group h-48 md:h-96 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
+         <img src="@/static/collection/20200816-OHT1I50196-L.jpg" loading="lazy" alt="Photo by Magicle" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
+ 
+         <div class="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
+ 
+         <span class="inline-block text-gray-200 text-xs md:text-sm border border-gray-500 rounded-lg backdrop-blur relative px-2 md:px-3 py-1 mr-3 mb-3">Pregnant</span>
+       </a>
+       <!-- image - end -->
+     </div>
+   </div>
+ </div>
+ </template>
+   
