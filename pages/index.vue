ee132030853c273c0fd1a00bec86d409471c07d5
Dune3844studio <dunestudio3844@gmail.com>
2023-01-10 21:37:43 +0900

    えfw

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
   </div>
  <div class="max-w-lg m-auto">
    <p class="mb-4">
-     うんこ ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
+     うんんこ ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat a
      magna non varius. Proin leo felis, euismod non porta eget, varius sit amet
      sapien. Maecenas in nulla at leo convallis consectetur id a sapien. Nulla
      nec pulvinar nisi. Vivamus non facilisis lacus, et volutpat libero. Nulla ac
